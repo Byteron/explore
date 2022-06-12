10989e759cf43668360c0dcdd0c914c62155ba9c
Aaron Winter <winter.aaron93@gmail.com>
2022-06-12 09:17:39 +0200

    remove gd stuff

--- src/combat/Damage.gd
+++
@@ -1,4 +0,0 @@
-extends Node
-class_name Damage
-
-export var amount := 10
--- src/physics/HitArea2D.gd
+++
@@ -1,8 +0,0 @@
-extends Area2D
-class_name HitArea2D
-
-signal hit(damage, origin)
-
-
-func hit(damage: Damage, origin: Vector2) -> void:
-	emit_signal("hit", damage, origin)
